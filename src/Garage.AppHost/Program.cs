ba706f9238132bc572cf7036ca168de558d5580b
André Silva <2493377+askpt@users.noreply.github.com>
2025-09-27 10:54:00 +0100

    Refactor flagd container configuration and update default variant for flags in flagd.json; improve feature flag handling in Home.razor
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/Garage.AppHost/Program.cs
+++ src/Garage.AppHost/Program.cs
@@ -5,8 +5,10 @@ var cache = builder.AddRedis("cache");
 var database = builder.AddSqlite("garage-db");
 
 var flagd = builder.AddContainer("flagd", "ghcr.io/open-feature/flagd:latest")
-    .WithBindMount("flags", "/flags")
-    .WithArgs("start", "--uri", "file:./flags/flagd.json")
+    // .WithBindMount("flags", "/flags")
+    .WithVolume("flags", "/flags_volume")
+    // .WithArgs("start", "--uri", "file:./flags/flagd.json")
+    .WithArgs("start", "--uri", "file:./flags_volume/flagd.json")
     .WithEndpoint(8013, 8013);
 
 var apiService = builder.AddProject<Projects.Garage_ApiService>("apiservice")
