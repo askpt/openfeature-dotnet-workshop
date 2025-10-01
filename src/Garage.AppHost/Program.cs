1162ec9d0df995f9579529720aa83ac7b822851e
André Silva <2493377+askpt@users.noreply.github.com>
2025-10-01 10:50:09 +0100

    Update flagd container configuration to use bind mount for flags and adjust URI arguments
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/Garage.AppHost/Program.cs
+++ src/Garage.AppHost/Program.cs
@@ -5,10 +5,10 @@ var cache = builder.AddRedis("cache");
 var database = builder.AddSqlite("garage-db");
 
 var flagd = builder.AddContainer("flagd", "ghcr.io/open-feature/flagd:latest")
-    // .WithBindMount("flags", "/flags")
-    .WithVolume("flags", "/flags_volume")
-    // .WithArgs("start", "--uri", "file:./flags/flagd.json")
-    .WithArgs("start", "--uri", "file:./flags_volume/flagd.json")
+    .WithBindMount("flags", "/flags")
+    // .WithVolume("flags", "/flags_volume")
+    .WithArgs("start", "--uri", "file:./flags/flagd.json")
+    // .WithArgs("start", "--uri", "file:./flags_volume/flagd.json")
     .WithEndpoint(8013, 8013);
 
 var apiService = builder.AddProject<Projects.Garage_ApiService>("apiservice")
