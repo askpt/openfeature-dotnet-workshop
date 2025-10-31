cb501271e8f540c7f9a093d9134cf189a8ee3075
André Silva <2493377+askpt@users.noreply.github.com>
2025-10-31 14:26:30 +0000

    Fix formatting in WinnerMapper and add newline at end of Winner model
    Signed-off-by: GitHub <noreply@github.com>

--- src/Garage.ApiService/Mappers/WinnerMapper.cs
+++ src/Garage.ApiService/Mappers/WinnerMapper.cs
@@ -1,5 +1,5 @@
-using DataModels = Garage.ApiService.Data.Models;
 using Riok.Mapperly.Abstractions;
+using DataModels = Garage.ApiService.Data.Models;
 using SharedModels = Garage.Shared.Models;
 
 [Mapper]
